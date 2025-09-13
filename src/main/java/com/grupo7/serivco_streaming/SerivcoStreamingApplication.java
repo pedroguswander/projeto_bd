46b5f60596c3e33b76d9003c8e4bae0ac7900b36
Pedro de Araújo Duarte Gusmão <168600233+pedroguswander@users.noreply.github.com>
2025-09-13 14:11:37 -0300

    Iniciando projeto

---
+++ src/main/java/com/grupo7/serivco_streaming/SerivcoStreamingApplication.java
@@ -0,0 +1,13 @@
+package com.grupo7.serivco_streaming;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SerivcoStreamingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SerivcoStreamingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/grupo7/serivco_streaming/SerivcoStreamingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grupo7.serivco_streaming;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SerivcoStreamingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
